0de9d2a96fa64e512af73eedbadd1801bfb6f735
Mokgelemokgothu <mokgelemokgothu@gmail.com>
2023-02-18 02:05:55 +0200

    program that prints all single digit numbers of base 10 starting from 0, followed by a new line

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,8 +5,8 @@
 /**
  *  * main - Entry point
  *   * Description: Print lowercase alphabet backwards followed by a newline.
- *    * Return: Always 0 (Success)
- *     * Author: Mokgelemokgothu
+ * Return: Always 0 (Success)
+ * Author: Mokgelemokgothu
  */
 
 int main(void)
@@ -15,7 +15,7 @@ int main(void)
 
 	for (z = 'z'; z >= 'a'; z--)
 	{
-			putchar(z);
+		putchar(z);
 	}
 	putchar('\n');
 
